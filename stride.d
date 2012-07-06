056bea9cf40d44f6ed9f3a51dfe48d2de87d1fe8
Dmitry Olshansky <dmitry.olsh@gmail.com>
2012-07-06 13:55:00 +0400

    new version of stride - switch based
    Looking at asm it became obvious to me that this benchmark is no good
    It's ALL about compiler able to inline or not certain things,
    e.g. only optStride and stride are properly inlined.
    Results depend on phase of moon and the mood of optimizer
    I gave up untill GDC compiles uni.d.

--- stride.d
+++ stride.d
@@ -1,7 +1,24 @@
-import uni, bench_suite, std.stdio, std.typetuple, std.algorithm;
+import uni, bench_suite, std.stdio, std.typetuple, std.range, std.algorithm;
 
-ubyte[] table;
-ubyte[] pktable;
+@system:
+
+__gshared ubyte[] table;
+__gshared ubyte[] pktable;
+immutable ubyte[] full_table = 
+[
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,	
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,	
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 
+	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 0, 0
+];
+alias Trie!(ubyte, char, sliceBits!(4, 7), sliceBits!(0, 4)) MyTrie;
+__gshared MyTrie trie1;
 
 pure auto genTable()
 {
@@ -24,7 +41,7 @@ pure auto genTable()
 
 //std.utf stride copied verbatim:
 
-uint stride(S)(in S str, size_t index) @safe pure
+uint stride(S)(in S str, size_t index) pure
     if (is(S : const(char[])))
 {
     immutable c = str[index];
@@ -34,7 +51,7 @@ uint stride(S)(in S str, size_t index) @safe pure
         return strideImpl(c, index);
  }
 
-private uint strideImpl(char c, size_t index) @trusted pure
+private uint strideImpl(char c, size_t index) pure
 in { assert(c & 0x80); }
 body
 {
@@ -67,16 +84,16 @@ size_t optiStride(C)(in C[] str, size_t index)
         return optiStrideImpl(c, index);
  }
 
-private size_t optiStrideImpl(char c, size_t index) @trusted pure
+private size_t optiStrideImpl(char c, size_t index) pure
 in { assert(c & 0x80); }
 body
 {
     static if (__traits(compiles, {import core.bitop; bsr(1);}))
     {
         import core.bitop;
-        immutable msbs = 7 - bsr(~c);
-        immutable test = msbs - 2;
-        if (test <= 4) return msbs;
+        size_t msbs = bsr(~c);
+        size_t test = 5 - msbs;
+        if (test <= 4) return 7 - msbs;
     }
     else
     {
@@ -91,13 +108,22 @@ body
     throw new UTFException("Invalid UTF-8 sequence", index);
 }
 
-//tabulated version
-size_t old_table(C)(in C[] str, size_t index)
+//tabulated versions
+size_t plain_table(C)(in C[] str, size_t index)
+{
+	immutable c = str[index];
+	size_t r = full_table[c];
+	if(!r)
+		throw new UTFException("Invalid UTF-8 sequence", index);
+	return r;
+}
+
+size_t if_table(C)(in C[] str, size_t index)
 {
 	immutable c = str[index];
     if (c < 0x80)
         return 1;
-	size_t r = table[str[index]-0x80];
+	size_t r = table[c-0x80];
 	if(!r)
 		throw new UTFException("Invalid UTF-8 sequence", index);
 	return r;
@@ -109,7 +135,7 @@ size_t packed_table(C)(in C[] str, size_t index)
 	immutable c = str[index];
     if (c < 0x80)
         return 1;
-	size_t r = (pktable[(str[index]-0x80)/2] >> (index&1)*4) & 0xF;
+	size_t r = (pktable[(c-0x80)/2] >> (index&1)*4) & 0xF;
 	if(!r)
 		throw new UTFException("Invalid UTF-8 sequence", index);
 	return r;
@@ -120,15 +146,69 @@ size_t trie_table(C)(in C[] str, size_t index)
 	immutable c = str[index];
     if (c < 0x80)
         return 1;
-	size_t r = trie1[str[index]-0x80];
+	size_t r = trie1[c-0x80];
 	if(!r)
 		throw new UTFException("Invalid UTF-8 sequence", index);
 	return r;
 }
 
-alias Trie!(ubyte, char, sliceBits!(4, 7), sliceBits!(0, 4)) MyTrie;
+struct KillIndex
+{
+	uint index;
+	ulong[4] pages;
+}
 
-MyTrie trie1;
+immutable KillIndex kidx = prepKillIndex();
+enum uint kill_reg = kidx.index;
+enum mask3 = 0b111, mask4 = 0b1111;
+
+pure KillIndex prepKillIndex()
+{
+	uint top=0;	
+	ulong[4] pages;
+	ulong tmp=0;
+	size_t top_i=0;
+	for(int i=0; i<128;){
+		immutable val = full_table[i+0x80];		
+		tmp = tmp | (cast(ulong)val << 4*(i%16));
+		i++;
+		if(i % 16 == 0)
+		{
+			size_t k=0;
+			for( ;k<top_i;k++)
+			{
+				if(pages[k] == tmp)
+					break;
+			}
+			//7 bits total, shift left 4  == 3 top bits for index register
+			top = top | (k  << 3*(i>>4)-3);
+			pages[k] = tmp; 
+			if(k == top_i)
+				top_i++;
+			tmp = 0;
+		}
+	}
+	assert(pages[3] != 0);
+	return KillIndex(top, pages);
+}
+
+size_t case_stride(C)(in C[] str, size_t index)
+{
+	switch(str[index] >> 3)
+	{
+	default: // ASCII with 0x80 + 
+		return 1;
+	case 0x80: .. case 0x87:
+	case 0x8F: //5 & 6 lengths are not supported any longer see unicode 6.0+
+		throw new UTFException("Invalid UTF-8 sequence", index);
+	case 0x88: .. case 0x8B:
+		return 2; 
+	case 0x8C: .. case 0x8D:
+		return 3;
+	case 0x8E:
+		return 4;
+	}
+}
 
 size_t siftThrough(alias func)(in char[] data)
 {
@@ -145,13 +225,29 @@ shared static this()
 	for(size_t i=0; i<table.length; i+=2)
 		pktable[i/2]  = cast(ubyte)(table[i] | (table[i+1]<<4));
 	trie1 = MyTrie(table.dup);                                          
-
-	//writeln(table);
+	for(int i=0;i<8;i++)
+		writef("%1d ", (kill_reg>> (3*i))& mask3);
+	writeln();
+	writeln("While table contains:");
+	for(int i=0;i<8;i++)
+	{
+		writefln("FULL TAB %d: %s", i, table[i*16..i*16+16]);
+		writef("KILL TRIE %d: ", i);
+		ulong t = kidx.pages[(kill_reg >> 3*(i)) & mask3];
+		for(size_t j=0;j<16;j++){
+			writef("%d, ", (t>>(4*j)) & mask4);
+		}
+		writeln();	
+	}
+	writefln("index: %x", kidx.index);
+	writefln("pages: [%( 0x%x, %)]", kidx.pages);
+	for(int i=0;i<128; i+=8)
+		writeln(i/8,": ",table[i..i+8]);
 }
 
 void strideTest(Result[] data)
 {
-	foreach(mtd; TypeTuple!(stride, optiStride, old_table, packed_table, trie_table))
+	foreach(mtd; TypeTuple!(stride, optiStride, plain_table, if_table, packed_table, trie_table, case_stride))
 	{
 		foreach(x; data)
 			bench!(siftThrough!(mtd))(mtd.stringof[0..10]~"-", x.name, x.data);
