e223c06d303a57862105e61b3d2df5ae277514ba
Dmitry Olshansky <dmitry.olsh@gmail.com>
2013-05-29 21:00:28 +0400

    strip \r

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
 for arch in 32 64
 do
-	for ver in my std
-	do
-		COMMAND="ldc2 -m$arch -of$ver$arch -O3 -release -d-version=$ver  -disable-boundscheck fast_stride.d"
-		echo $COMMAND
-		$COMMAND
-	done
+    for ver in my std
+    do
+	DMD="dmd -O -release -version=$ver -noboundscheck -inline fast_stride.d -m$arch "
+	LDC="ldc2  -O3 -release -d-version=$ver  -disable-boundscheck fast_stride.d -m$arch "
+        OUT="-ofdmd_$ver$arch"
+        echo $DMD $OUT
+        $DMD $OUT
+	OUT="-ofldc_$ver$arch"
+	echo $LDC $OUT
+	$LDC $OUT
+    done
 done
--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+for comp in dmd_ ldc_
+do
 for arch in 32 64
 do
 	for ver in my std
 	do
-		echo $ver$arch
+		echo $comp$ver$arch
 		for i in 1 2 3
 		do
-			./$ver$arch $1
+			./$comp$ver$arch $1
 		done
 	done
+done
 done
